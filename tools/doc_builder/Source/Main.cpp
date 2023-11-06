c27f018a64025fdd58daa6be7f201a5724b15e5f
Tobias Hienzsch <post@tobias-hienzsch.de>
2023-11-06 18:14:32 +0100

    Add CMake target for tools/doc_builder

--- tools/doc_builder/Source/Main.cpp
+++ tools/doc_builder/Source/Main.cpp
@@ -1,15 +1,4 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-    It contains the basic startup code for a Juce application.
-
-  ==============================================================================
-*/
-
-#include "../JuceLibraryCode/JuceHeader.h"
-
+#include "JuceHeader.h"
 
 struct StandardLogger : public hise::DatabaseCrawler::Logger
 {
